30c1c4a2935125744a758eaa401af459149d0d22
acossta <nico@propeldata.com>
2024-11-04 11:52:13 -0800

    adjust padding

--- src/app/data-grid/page.tsx
+++ src/app/data-grid/page.tsx
@@ -2,7 +2,7 @@ import { DataGrid, Sort, Flex } from "@propeldata/ui-kit";
 
 export default async function CounterExample() {
   return (
-    <Flex p="7" align="center" justify="center">
+    <Flex p="3" align="center" justify="center">
       <DataGrid
         query={{
           columns: ["timestamp", "taco_name", "taco_unit_price"],
